1369431e77275ae17bafab157608ab4e03401cad
Hector Bastos <hbastosc@users.noreply.github.com>
2022-11-05 00:15:06 -0600

    more changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,3 @@
--- Plugins Install
-require 'hbastosc.plugins'
-
--- GUI Plugins
-require 'hbastosc.font'
-require 'hbastosc.theme'
-require 'hbastosc.treesitter'
 require 'hbastosc.options'
-
--- LSP and Intellisense
-require 'hbastosc.treesitter'
-require 'hbastosc.lspconfig'
+require 'hbastosc.maps'
+require 'hbastosc.plugins'
