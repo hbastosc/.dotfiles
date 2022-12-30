7251e384190181ca0a8eb30cddb53aa6a564b384
Hector Bastos <hbastosc@users.noreply.github.com>
2022-12-30 12:26:56 -0600

    disable Enter to confirm

--- config/nvim/after/plugin/lsp-zero.lua
+++ config/nvim/after/plugin/lsp-zero.lua
@@ -6,14 +6,28 @@ lsp.set_preferences({
   configure_diagnostics = false,
 })
 
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<tab>'] = cmp.mapping.confirm({ select = true }),
+  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
+
+-- disable Enter to confirm
+cmp_mappings['<CR>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
