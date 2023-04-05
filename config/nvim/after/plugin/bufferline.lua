7e68749a45a30e996adf00bceaa01a54ed55df4f
Hector Bastos <hecbc23@gmail.com>
2023-04-05 15:37:48 -0600

    lots changes

--- config/nvim/after/plugin/bufferline.lua
+++ config/nvim/after/plugin/bufferline.lua
@@ -1,29 +1,29 @@
 local ok, bufferline = pcall(require, "bufferline")
 if not ok then
-  return
+    return
 end
 
 diagnostics_indicator = function(count, level, diagnostics_dict, context)
-  local s = " "
-  for e, n in pairs(diagnostics_dict) do
-    local sym = e == "error" and " "
-      or (e == "warning" and " " or "" )
-    s = s .. n .. sym
-  end
-  return s
+    local s = " "
+    for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+            or (e == "warning" and " " or "")
+        s = s .. n .. sym
+    end
+    return s
 end
 
-bufferline.setup{
-  options = {
-    diagnostics = "nvim_lsp",
-    diagnostics_indicator = diagnostics_indicator,
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        highlight = "Directory",
-        text_align = "left"
-      },
+bufferline.setup {
+    options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = diagnostics_indicator,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                text_align = "left"
+            },
+        },
     },
-  },
 }
--- config/nvim/lua/hbastosc/lazynvim.lua
+++ config/nvim/lua/hbastosc/lazynvim.lua
@@ -1,69 +1,67 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "kyazdani42/nvim-web-devicons",
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-treesitter/playground",
-  "tpope/vim-fugitive",
-  "nvim-lualine/lualine.nvim",
-  "mbbill/undotree",
-  "theprimeagen/harpoon",
-  "akinsho/bufferline.nvim",
-  "jose-elias-alvarez/null-ls.nvim",
-  {
-    "rose-pine/neovim",
-     name = "rose-pine",
-     lazy = false,
-     priority = 1000,
-     config = function()
-       require("rose-pine").setup({
-         disable_background = true,
-         disable_italics = false,
-       })
-       vim.cmd("colorscheme rose-pine")
-     end
-  },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
-    dependencies = {
-      -- LSP Support
-      {"neovim/nvim-lspconfig"},             -- Required
-      {"williamboman/mason.nvim"},           -- Optional
-      {"williamboman/mason-lspconfig.nvim"}, -- Optional
-
-      -- Autocompletion
-      {"hrsh7th/nvim-cmp"},         -- Required
-      {"hrsh7th/cmp-nvim-lsp"},     -- Required
-      {"hrsh7th/cmp-buffer"},       -- Optional
-      {"hrsh7th/cmp-path"},         -- Optional
-      {"saadparwaiz1/cmp_luasnip"}, -- Optional
-      {"hrsh7th/cmp-nvim-lua"},     -- Optional
-
-      -- Snippets
-      {"L3MON4D3/LuaSnip"},             -- Required
-      {"rafamadriz/friendly-snippets"}, -- Optional
-    }
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {"nvim-lua/plenary.nvim"}
-  },
-  {
+    "kyazdani42/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/playground",
+    "tpope/vim-fugitive",
+    "nvim-lualine/lualine.nvim",
+    "mbbill/undotree",
+    "theprimeagen/harpoon",
+    "akinsho/bufferline.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    "folke/neodev.nvim",
     "lewis6991/gitsigns.nvim",
-    config = function ()
-      require("gitsigns").setup()
-    end
-  }
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                disable_italics = false,
+            })
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            -- LSP Support
+            { "williamboman/mason.nvim" },           -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },         -- Required
+            { "hrsh7th/cmp-nvim-lsp" },     -- Required
+            { "hrsh7th/cmp-buffer" },       -- Optional
+            { "hrsh7th/cmp-path" },         -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" },     -- Optional
+
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },             -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
+
+            -- Useful status updates for LSP
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim',                opts = {} },
+        }
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
 })
