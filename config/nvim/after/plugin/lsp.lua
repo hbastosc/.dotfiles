d72f010ebab64a92fb7c3a61ac7320208cd11153
Hector Bastos <hecbc23@gmail.com>
2023-04-10 22:18:37 -0600

    added custom binds

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -15,11 +15,9 @@ local on_attach = function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-    nmap('[d', vim.diagnostic.goto_next, '')
-    nmap(']d', vim.diagnostic.goto_prev, '')
     nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
@@ -43,6 +41,13 @@ local on_attach = function(_, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
+
+    -- Custom binds
+    nmap('[d', vim.diagnostic.goto_next, '')
+    nmap(']d', vim.diagnostic.goto_prev, '')
+    nmap('<leader>vca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    nmap('<leader>vrr', vim.lsp.buf.references, '')
+    nmap('<leader>vrn', vim.lsp.buf.rename, '')
 end
 
 -- Enable the following language servers
