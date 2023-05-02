59f427e1196166cb7a5003ac61c0fd2d617445b2
Hector Bastos <hecbc23@gmail.com>
2023-05-02 08:46:27 -0600

    fixed import

--- config/nvim/lua/hbastosc/lazynvim.lua
+++ config/nvim/lua/hbastosc/lazynvim.lua
@@ -79,6 +79,6 @@ require("lazy").setup({
     -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
     --       These are some example plugins that I've included in the kickstart repository.
     --       Uncomment any of the lines below to enable them.
-    require 'autoformat',
+    require 'hbastosc.autoformat',
     -- require 'kickstart.plugins.debug',
 })
